e318cd7195546d1bdf398ea3b50ff9480729407f
Simran <64429491+simran-aksj@users.noreply.github.com>
2025-06-26 00:20:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+    
+        unordered_map <int,int>mpp;
+        int rem;
+        for(int i=0;i<arr.size();i++)
+        {
+            rem= target-arr[i];
+            if(mpp.find(rem)!=mpp.end())
+               return true;
+           mpp[arr[i]]=i;
+            
+        }
+      return false;
+    }
+};
