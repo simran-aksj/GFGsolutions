fa31deec776b3d8f26d20953929d35bcdfc40d10
Simran <64429491+simran-aksj@users.noreply.github.com>
2025-06-25 12:27:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int longestUniqueSubstring(string &s) {
+        // code
+        unordered_map<char,int>mpp;
+        int n=s.size();
+        int i=0,j=0,cnt=0;
+        if(n==1)return 1;
+        if(n==0)return 0;    ///////j-i+1== window size()
+        while(j<n)
+        {
+            mpp[s[j]]++;
+            if(mpp.size()== (j-i+1))
+           { cnt=max(cnt,j-i+1);// kyuki maximum window size return karna h;
+             j++;
+               
+           }
+              else if(mpp.size()<j-i+1)
+                {
+                     while(mpp.size()<j-i+1){
+                          mpp[s[i]]--;
+                          if (mpp[s[i]] == 0) 
+                           mpp.erase(s[i]);
+                          i++;
+                     }
+                   j++;
+                    
+                }
+        }
+      return cnt;    
+    }
+};
