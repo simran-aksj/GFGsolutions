60f9a7986538d19b3354553a963b2ac909af3f1a
Simran <64429491+simran-aksj@users.noreply.github.com>
2025-08-13 16:10:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Tywin's War Strategy/tywins-war-strategy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minSoldiers(vector<int>& arr, int k) {
+        // code here
+           int luckySoldiers = 0;
+        priority_queue<int, vector<int>, greater<int> > pq;
+        int n= arr.size();
+        for(int i=0; i<n; i++){
+            if(arr[i]%k==0){
+                luckySoldiers++;
+            }else{
+                int soldiersToBeAdded = k - (arr[i]%k);
+                pq.push(soldiersToBeAdded);
+            }
+        }
+        int totalToBeAdded = 0;
+        while((luckySoldiers<ceil(n/2.0)) && !pq.empty()){
+            totalToBeAdded += pq.top();
+            pq.pop();
+            luckySoldiers++;
+        }
+        return totalToBeAdded;
+    }
+};
